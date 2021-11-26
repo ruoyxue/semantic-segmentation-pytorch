d64e9ec0555d9a9d3f029aaf54323dc4f81bbc1b
xueruoyao <1642216761@qq.com>
2021-11-26 16:41:24 +0800

    revise fetcher

--- code/utils/testloader.py
+++ code/utils/testloader.py
@@ -1,17 +1,18 @@
 """
 Taking into consideration that pytorch dataset and dataloader
 seem to be clumsy for daily self-use, I would like to construct a
-specialised light-weight trainloader for data preparation
+specialised light-weight dataloader for training and testing
 
-:TODO need to adjust fetcher to make it more readable
+TODO: need to adjust fetcher to make it more readable
+TODO: add num_workers for data load parallelization
 """
 import logging
-
 import numpy as np
 import torch
 import cv2
 import os
 import datetime
+from torchvision import transforms
 from typing import List, Callable, Optional, Tuple
 import rasterio
 import imagesize
@@ -24,10 +25,8 @@ class ComputerVisionTestLoader:
     :param batch_size: how many samples per batch to load
     :param preprocessing: if not None, use preprocessing function after loading
 
-    Note: we save all chip images information in self.chip_info in form of
-          (image_index, height_coord, width_coord),
-          where image_index refers to the image that chipped image belongs to (in terms of os.listdir)
-          , (height_coord, width_coord) denotes the upper-left point coordinate of chipped image
+    Note: we save all chip images information in self.chip_information in form of
+          (image_index, height_coord, width_coord)
     """
     whole_image: torch.tensor = None
     count: torch.tensor = None
@@ -43,11 +42,15 @@ class ComputerVisionTestLoader:
         self.n_class = n_class
         self.batch_size = batch_size
         self.preprocessing = preprocessing
-        self.chip_info = []
-        self.prepare_chip_info()
+        self.chip_information = []
+        self.normalisation = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(89.8, 91.3, 89.9), std=(68.0, 65.6, 66.4))
+        ])
+        self.prepare_chip_information()
 
-    def prepare_chip_info(self):
-        """ prepare chip_info """
+    def prepare_chip_information(self):
+        """ prepare chip information, saved in self.chip_information """
         if not os.path.exists(self.image_path):
             raise FileNotFoundError("testloader image path not exists")
         elif len(os.listdir(self.image_path)) == 0:
@@ -57,50 +60,48 @@ class ComputerVisionTestLoader:
             self.image_name_list.append(image_name)
             self.image_path_list.append(os.path.join(self.image_path, image_name))
 
+        # find all possible upper left coordinates for chipped images,
+        # save as (image_index, height_coord, width_coord)
         for count in range(len(self.image_path_list)):
             width, height = imagesize.get(self.image_path_list[count])
             assert width >= self.chipsize and height >= self.chipsize,\
-                f"chipsize doesn't work for {self.image_path_list[count]} with size of ({height}, {width})"
+                f"chipsize {self.chipsize} doesn't work for " \
+                f"{self.image_path_list[count]} with size ({height}, {width})"
 
-            list_i = np.unique(np.array(list(range(0, (height - self.chipsize), self.stride)) +
-                                        [height - self.chipsize, 0]))
-            list_j = np.unique(np.array(list(range(0, (width - self.chipsize), self.stride)) +
-                                        [width - self.chipsize, 0]))
-            for i in list_i:
-                for j in list_j:
-                    self.chip_info.append((count, i, j))
+            list_height = np.unique(np.array(list(range(0, (height-self.chipsize), self.stride)) +
+                                             [height-self.chipsize, 0]))  # height coord
+            list_width = np.unique(np.array(list(range(0, (width-self.chipsize), self.stride)) +
+                                            [width-self.chipsize, 0]))  # width coord
+            for i in list_height:
+                for j in list_width:
+                    self.chip_information.append((count, i, j))
 
     def sampler(self):
-        """ start_index, end_index for chip images in self.chip_info """
-        for i in range(0, len(self), self.batch_size):
-            yield i, min(i + self.batch_size, len(self))
+        """ start_index, end_index of self.chip_info for different batches"""
+        for i in range(0, len(self.chip_information), self.batch_size):
+            yield i, min(i + self.batch_size, len(self.chip_information))
 
     def fetcher(self, start_index: int, end_index: int):
         """
-        return chipped images of given indices, with possible preprocessing,
-        in shape (batch_size, channel, height, width)
+        return normalised chipped images of given indices,
+        shape (batch_size, channel, height, width)
         """
-        chipped_images, info = [], []
-        chipped_info = self.chip_info[start_index: end_index]
-        # split chipped_info in terms of their whole image index
-        image_indices = np.unique(np.array(chipped_info)[:, 0])  # get whole image index
-        for image_index in image_indices:
-            info.append([])
-        for i in range(self.batch_size):
-            info[chipped_info[i][0] - min(image_indices)].append(chipped_info[i])
-        # load whole image and chip
-        for element in info:
-            image = self.load(self.image_path_list[element[0][0]])
-            if self.preprocessing is not None:
-                image = self.preprocessing(image)
-            image = np.rollaxis(image, 2, 0)
-            for index, height_coord, width_coord in element:
-                chipped_images.append(torch.tensor(
-                      image[:, height_coord: height_coord + self.chipsize, width_coord: width_coord + self.chipsize],
-                      dtype=torch.float))
-        return torch.stack(chipped_images, dim=0), np.array(chipped_info)
+        chip_info = self.chip_information[start_index: end_index]
+        index = chip_info[0][0]  # record current image index (large image)
+        image = self.normalisation(self.load(self.image_path_list[index]))  # (channel, height, width)
+        distributed_images = []  # record chipped images to be distributed
 
-    def stitcher(self, preds, info, last_batch_flag: bool = False):
+        # load image and chip, info: (image_index, height_coord, width_coord)
+        for info in chip_info:
+            if info[0] != index:  # means need to load another image to chip
+                index = info[0]
+                image = self.normalisation(self.load(self.image_path_list[index]))
+            distributed_images.append(
+                image[:, info[1]: info[1]+self.chipsize, info[2]: info[2]+self.chipsize])
+
+        return torch.stack(distributed_images, dim=0), torch.tensor(chip_info)
+
+    def stitcher(self, preds: torch.Tensor, info, last_batch_flag: bool = False):
         """ stitch the preds together and return whole predicted image and its name
         :param preds: predictions of chipped images
         :param info: information of chipped images
@@ -112,14 +113,15 @@ class ComputerVisionTestLoader:
         half_stride = self.stride // 2
         kernel = np.ones((self.chipsize, self.chipsize), dtype=np.float32)
         kernel[half_stride:-half_stride, half_stride:-half_stride] = 10
-        for i in range(info.shape[0]):
-            # initialisation for first batch
-            if self.current_image_index is None:
-                self.current_image_index = info[0, 0]
-                width, height = imagesize.get(self.image_path_list[self.current_image_index])
-                self.count = np.zeros([height, width])
-                self.whole_image = np.zeros([self.n_class, height, width])
 
+        # initialisation for first batch
+        if self.current_image_index is None:
+            self.current_image_index = info[0, 0]
+            width, height = imagesize.get(self.image_path_list[self.current_image_index])
+            self.count = np.zeros([height, width])
+            self.whole_image = np.zeros([self.n_class, height, width])
+
+        for i in range(info.shape[0]):
             if info[i, 0] == self.current_image_index:
                 # add chipped image to whole_image
                 self.whole_image[:, info[i, 1]: info[i, 1]+self.chipsize, info[i, 2]: info[i, 2]+self.chipsize] += \
@@ -140,6 +142,7 @@ class ComputerVisionTestLoader:
                 self.count[info[i, 1]: info[i, 1] + self.chipsize, info[i, 2]: info[i, 2] + self.chipsize] += kernel
 
         if last_batch_flag is True:
+            print("last image")
             self.whole_image /= self.count
             yield self.whole_image, self.image_name_list[self.current_image_index]
 
@@ -154,7 +157,7 @@ class ComputerVisionTestLoader:
             yield self.fetcher(start_index, end_index)
 
     def __len__(self):
-        return len(self.chip_info)
+        return len(self.chip_information)
 
 
 class PNGTestloader(ComputerVisionTestLoader):
