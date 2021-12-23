2179d9881e0ed2ec1a1b33da0c18169f4c3679f4
xueruoyao <1642216761@qq.com>
2021-12-23 22:40:20 +0800

    test diff

--- code/train.py
+++ code/train.py
@@ -64,6 +64,7 @@ def valider(train_args: argparse, logger):
     train_args.model.train()
     return evaluator.get_metrics()
 
+# therwefeasfdsf
 
 def trainer(train_args: argparse, logger):
     save_model_path = os.path.join(train_args.exp_path, "model_saved")
