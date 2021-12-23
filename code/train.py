7c953d854b7614dd891311f6931df0610567eaf4
xueruoyao <1642216761@qq.com>
2021-12-23 22:35:23 +0800

    test diff

--- code/train.py
+++ code/train.py
@@ -64,7 +64,7 @@ def valider(train_args: argparse, logger):
     train_args.model.train()
     return evaluator.get_metrics()
 
-# here's the diff
+# here's the di
 
 
 def trainer(train_args: argparse, logger):
